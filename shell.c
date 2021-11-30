51913ffe669fe7b33e6c11a840d9c96699cfb38f
ikenton <89114492+ikenton@users.noreply.github.com>
2021-11-29 17:20:28 -0800

    lsh loop & lsh read line functions

--- shell.c
+++ shell.c
@@ -1,2 +1,60 @@
+#include <stdio.h>
+#define LSH_RL_BUFSIZE 1024
+#define EXIT_SUCCESS
+#define EXIT_FAILURE
 
+int main(int argc, char **argv){
+    lsh_loop();
+    return EXIT_SUCCESS;
+}
+
+void lsh_loop(void){
+    char *line;
+    char **args;
+    int status;
+
+    do{
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+        
+        free(line);
+        free(args);
+    }while(status);
+}
+
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char)*bufsize);
+    int c;
+
+    if(!buffer){
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+        c = getchar();
+        if(c = EOF || c == '\n'){
+            buffer[position] = '\0';
+            return buffer;
+        }else{
+            buffer[position] = c;
+        }
+        position++;
+    
+
+        if(position >= bufsize){
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if(!buffer){
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
 
