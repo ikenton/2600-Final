20f1a1ea398c2d9e62bb6fb9f8a18a470404642b
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-05 13:31:34 -0800

    Column of tildes on the left hand side

--- kilo.c
+++ kilo.c
@@ -83,4 +83,15 @@ void editorProcesKeypresses(){
 void editorRefreshScreen(){
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1B[h", 3);
+
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
+
+void editorDrawRows(){
+    int y;
+    for(y = 0; y < 24; y++){
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
