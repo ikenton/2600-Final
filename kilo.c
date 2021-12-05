14f656fc79a51ce50b31d455b1aea285b6f9d648
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-05 13:28:58 -0800

    clear screen on exit

--- kilo.c
+++ kilo.c
@@ -46,6 +46,8 @@ void disableRawMode() {
 }
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
@@ -69,6 +71,8 @@ void editorProcesKeypresses(){
 
         switch(c){
             case CTRL_KEY('q'):
+                write(STDOUT_FILENO, "\x1b[2J", 4);
+                write(STDOUT_FILENO, "\x1b[H", 3);
                 exit(0);
                 break;
         }
