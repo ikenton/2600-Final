acde5dbab6f67c91effff609a4bc0a53775264dc
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-05 13:01:07 -0800

    display keypresses, updated main.c

--- kilo.c
+++ kilo.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 struct termios orig_termios;
 
@@ -8,7 +10,13 @@ int main(){
     enableRawMode();
 
     char c; 
-    while(read(STDIN_FILENO, &c, 1)==1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
 
@@ -17,7 +25,8 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | CANON);
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
