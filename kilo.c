cd72a89e21a2df8289a3014c4f1a71b94b098a2d
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-05 13:13:51 -0800

    error handling, implemented die()

--- kilo.c
+++ kilo.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
-
+#include <errno.h>
 struct termios orig_termios;
 
 int main(){
@@ -12,7 +12,7 @@ int main(){
     char c; 
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
@@ -24,6 +24,7 @@ int main(){
 }
 
 void enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
 
@@ -35,9 +36,15 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
 }
