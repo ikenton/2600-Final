d0f0d2975bcb2bf0963cd96ad95d242a63ad101e
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-05 20:18:25 -0800

    recognizes delete key but doesn't do anything yet

--- kilo.c
+++ kilo.c
@@ -14,6 +14,7 @@ enum editorKey{
     ARROW_LEFT =1000,
     ARROW_RIGHT,
     ARROW_UP,
+    DEL_KEY,
     ARROW_DOWN,
     HOME_KEY,
     END_KEY,
@@ -95,6 +96,7 @@ int editorReadKey(){
                 if(seq[2] == '~'){
                     switch(seq[1]){
                         case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
                         case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
