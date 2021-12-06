bc6a6c123b966644fc331b7f0cd46608cb5fc42c
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-05 19:55:44 -0800

    implemented arrow keys

--- kilo.c
+++ kilo.c
@@ -9,6 +9,13 @@
 /*defines*/
 #define CTRL_KEY(k)((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
+
+enum editorkey{
+    ARROW_LEFT =1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
 /* data */
 
 struct editorConfig{
@@ -66,14 +73,30 @@ void die(const char *s) {
     exit(1);
 }
 
-char editorReadKey(){
+int editorReadKey(){
     int nread;
     char c;
 
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if(nread == -1 && errno != EAGAIN) die("read");
     }
-    return c;
+
+    if(c == '\x1b'){
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+            }
+        }
+        return '\x1b';
+    }else{
+        return c;
+    }
 }
 
 int getWindowSize(int *rows, int *cols){
@@ -107,10 +130,12 @@ int getCursorPosition(int *rows, int *cols){
     if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
     return 0;
 }
+
+
 /* input */
 
 void editorProcesKeypresses(){
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch(c){
         char c = editorReadKey();
@@ -121,28 +146,28 @@ void editorProcesKeypresses(){
                 write(STDOUT_FILENO, "\x1b[H", 3);
                 exit(0);
                 break;
-            case 'w':
-            case 's':
-            case 'a':
-            case 'd':
+            case ARROW_UP:
+            case ARROW_DOWN:
+            case ARROW_LEFT:
+            case ARROW_RIGHT:
                 editorMoveCursor(c);
                 break;
         }
     }
 }
 
-void editorMoveCursor(char key){
+void editorMoveCursor(int key){
     switch(key){
-        case 'a':
+        case ARROW_LEFT:
             E.cx--;
             break;
-        case 'd':
+        case ARROW_RIGHT:
             E.cx++;
             break;
-        case 'w':
+        case ARROW_UP:
             E.cy--;
             break;
-        case 's':
+        case ARROW_DOWN:
             E.cy++;
             break;
 
