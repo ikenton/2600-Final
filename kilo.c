9a0a93092916f4d9bcc319ddb1fba7522438383c
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-06 11:03:36 -0800

    save to disk, new prototype section

--- kilo.c
+++ kilo.c
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <time.h>
 #include <stdarg.h>
+#include <fcntl.h>
 
 /*defines*/
 #define CTRL_KEY(k)((k) & 0x1f)
@@ -63,7 +64,7 @@ int main(int argc, char *argv[]){
         editorOpen(argv[1]);
     }
     //WEIRD ERROR HERE
-
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
     editorSetStatusMessage("HELP: Ctrl-Q = quit");
     while (1) {
         editorRefreshScreen();
@@ -87,6 +88,9 @@ void initEditor(){
     if(getWindowSize(&E.screenrows, &E.screencols)==-1) die("getWindowSize");
     E.screenrows -= 2;
 }
+
+/*** prototypes ***/
+void editorSetStatusMessage(const char *fmt, ...);
 
 /* terminal */
 void enableRawMode() {
@@ -213,6 +217,9 @@ void editorProcessKeypress(){
                 write(STDOUT_FILENO, "\x1b[2J", 4);
                 write(STDOUT_FILENO, "\x1b[H", 3);
                 exit(0);
+                break;
+            case CTRL_KEY('s'):
+                editorSave();
                 break;
             case HOME_KEY:
                 E.cx =0;
@@ -489,6 +496,22 @@ void editorInsertChar(int c) {
 }
 
 /* file i/o */
+char *editorRowsToString(int *buflen) {
+    int totlen = 0;
+    int j;
+    for (j = 0; j < E.numrows; j++)
+        totlen += E.row[j].size + 1;
+    *buflen = totlen;
+    char *buf = malloc(totlen);
+    char *p = buf;
+    for (j = 0; j < E.numrows; j++) {
+        memcpy(p, E.row[j].chars, E.row[j].size);
+        p += E.row[j].size;
+        *p = '\n';
+        p++;
+    }
+    return buf;
+}
 void editorOpen(char *filename){
     free(E.filename);
     E.filename = strdup(filename);
@@ -507,4 +530,24 @@ void editorOpen(char *filename){
     }
     free(line);
     fclose(fp);
+}
+
+void editorSave() {
+    if (E.filename == NULL) return;
+    int len;
+    char *buf = editorRowsToString(&len);
+    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+    if (fd != -1) {
+        if (ftruncate(fd, len) != -1) {
+            if (write(fd, buf, len) == len) {
+                close(fd);
+                free(buf);
+                editorSetStatusMessage("%d bytes written to disk", len);
+                return;
+            }
+        }
+        close(fd);
+    }
+    free(buf);
+    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
