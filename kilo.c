5f0e4960eb9e74231e1657d7275b521e1ae218d8
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-05 21:24:25 -0800

    horizontal and vertical scrolling

--- kilo.c
+++ kilo.c
@@ -34,10 +34,12 @@ typedef struct erow{
 
 struct editorConfig{
     int cx, cy;
+    int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow *row;
     struct termios orig_termios;
 };
 struct editorConfig E;
@@ -46,7 +48,7 @@ struct editorConfig E;
 int main(int argc, char *argv[]){
     enableRawMode();
     initEditor();
-    if (argc >= 2) {
+    if (argc >= 2){
         editorOpen(argv[1]);
     }
     //WEIRD ERROR HERE
@@ -60,7 +62,10 @@ int main(int argc, char *argv[]){
 void initEditor(){
     E.cx = 0;
     E.cy = 0;
+    E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
+    E.row = NULL;
     if(getWindowSize(&E.screenrows, &E.screencols)==-1) die("getWindowSize");
 }
 
@@ -220,9 +225,7 @@ void editorMoveCursor(int key){
             }
             break;
         case ARROW_RIGHT:
-            if (E.cx != E.screencols - 1) {
                 E.cx++;
-            }
             break;
         case ARROW_UP:
             if (E.cy != 0) {
@@ -230,7 +233,7 @@ void editorMoveCursor(int key){
             }
             break;
         case ARROW_DOWN:
-            if (E.cy != E.screenrows - 1) {
+            if (E.cy != E.numrows) {
                 E.cy++;
             }
             break;
@@ -240,6 +243,7 @@ void editorMoveCursor(int key){
 
 /* output*/
 void editorRefreshScreen(){
+    editorScroll();
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab,"\x1b[?25l", 6 );
@@ -248,7 +252,7 @@ void editorRefreshScreen(){
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf),"\x1b[%d;%dH",E.cy +1, E.cx +1 );
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,(E.cx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[H", 3);
@@ -257,10 +261,25 @@ void editorRefreshScreen(){
     abFree(&ab);
 }
 
+void editorScroll(){
+    if(E.cy < E.rowoff){
+        E.rowoff = E.cy;
+    }
+    if(E.cy >= E.rowoff + E.screenrows){
+        E.rowoff = E.cy - E.screenrows +1;
+    }
+    if (E.cx < E.coloff) {
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols) {
+        E.coloff = E.cx - E.screencols + 1;
+    }
+}
 void editorDrawRows(struct abuf *ab){
     int y;
     for(y = 0; y < E.screenrows; y++){
-        if(y >= E.numrows){
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows) {
             if (E.numrows == 0 && y == E.screenrows / 3) {
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
@@ -276,9 +295,10 @@ void editorDrawRows(struct abuf *ab){
                 abAppend(ab, "~", 1);
             }
         }else{
-            int len = E.row.size;
-            if(len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row.chars, len);
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0) len = 0;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -303,10 +323,23 @@ void abAppend(struct abuf *ab, const char *s, int len){
     ab->len+=len;
 }
 
-void abFree(struct abuf *ab){
-    free(ab->b);
+void editorAppendRow(char *s, size_t len) {
+    E.row.size = len;
+    E.row.chars = malloc(len + 1);
+    memcpy(E.row.chars, s, len);
+    E.row.chars[len] = '\0';
+    E.numrows = 1;
 }
-
+/* row operations */
+void editorAppendRow(char *s, size_t len) {
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
 /* file i/o */
 void editorOpen(char *filename){
     FILE *fp = fopen(filename, "r");
@@ -315,17 +348,11 @@ void editorOpen(char *filename){
     char *line = NULL;
     size_t linecap = 0;
     ssize_t linelen = 0;
-    linelen = getline(&line, &linecap, fp);
-    if(linelen != -1){
+    
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
         while(linelen > 0 && (line[linelen -1] == '\n' || line[linelen -1] == '\r'))
             linelen--;
-        char *line = "Hello, world!";
-        ssize_t linelen = 13;
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen + 1);
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0';
-        E.numrows = 1; 
+        editorAppendRow(line,linelen);
     }
     free(line);
     fclose(fp);
