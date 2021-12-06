c6d8faccd8e582524508e6dfe343b2b8601df313
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-05 19:30:40 -0800

    append buffer, replace writes()

--- kilo.c
+++ kilo.c
@@ -131,13 +131,31 @@ void editorRefreshScreen(){
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
-void editorDrawRows(){
+void editorDrawRows(struct abuf *ab){
     int y;
     for(y = 0; y < E.screenrows; y++){
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
         if (y < E.screenrows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
+/* append buffer */
+struct abuf {
+  char *b;
+  int len;
+};
+#define ABUF_INIT {NULL, 0}
 
+void abAppend(struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab->len + len);
+
+    if(new == NULL) return;
+    memccpy(&new[ab -> len], s, len);
+    ab->b = new;
+    ab->len+=len;
+}
+
+void abFree(struct abuf *ab){
+    free(ab->b);
+}
