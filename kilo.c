16318a61a74bc9a084730f603a30ca6c9a700992
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-07 19:58:53 -0800

    colorful strings

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@
 #define KILO_TAB_STOP 8
 #define KILO_QUIT_TIMES 3
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 enum editorKey{
     BACKSPACE = 127,
     ARROW_LEFT =1000,
@@ -35,7 +36,9 @@ enum editorKey{
 
 enum editorHighlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_STRING,
+    HL_NUMBER,
+    HL_MATCH
 };
 
 /* data */
@@ -78,7 +81,7 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
-        HL_HIGHLIGHT_NUMBERS
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
 };
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
@@ -553,7 +556,7 @@ void editorFindCallback(char *query, int key) {
 
             saved_hl_line = current;
             saved_hl = malloc(row->rsize);
-            memcpy(saved_hl, row->hl, row->rsize);
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
@@ -606,11 +609,30 @@ void editorUpdateSyntax(erow *row) {
 
     if (E.syntax == NULL) return;
     int prev_sep = 1;
+    int in_string = 0;
 
     int i=0;
     while (i < row->rsize) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+            if (in_string) {
+                row->hl[i] = HL_STRING;
+                if (c == in_string) in_string = 0;
+                i++;
+                prev_sep = 1;
+                continue;
+            } else {
+                if (c == '"' || c == '\'') {
+                    in_string = c;
+                    row->hl[i] = HL_STRING;
+                    i++;
+                    continue;
+                }
+            }
+        }   
+
         if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
             if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||(c == '.' && prev_hl == HL_NUMBER)) {
                 row->hl[i] = HL_NUMBER;
@@ -626,7 +648,9 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
     switch (hl) {
+        case HL_STRING: return 35;
         case HL_NUMBER: return 31;
+        case HL_MATCH: return 34;
         default: return 37;
     }
 }
