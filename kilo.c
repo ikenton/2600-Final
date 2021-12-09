e11269db363e9a268c42d984271b0002c32b9e4b
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-09 12:39:01 -0800

    moved editorRefresh to be below editorDrawRows to fix wimplict error foreditorDrawRows

--- kilo.c
+++ kilo.c
@@ -582,36 +582,6 @@ void editorScroll(){
     }
 }
 
-void editorRefreshScreen(){
-    editorScroll();
-    struct abuf ab = ABUF_INIT;
-
-    abAppend(&ab,"\x1b[?25l", 6 );
-    abAppend(&ab, "\x1b[H", 3);
-
-    editorDrawRows(&ab);
-    editorDrawStatusBar(&ab);
-    editorDrawMessageBar(&ab);
-
-    char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,(E.rx - E.coloff) + 1);
-    abAppend(&ab, buf, strlen(buf));
-
-    abAppend(&ab, "\x1b[H", 3);
-    abAppend(&ab, "\x1b[?25h", 6);
-    write(STDOUT_FILENO, ab.b, ab.len);
-    abFree(&ab);
-}
-
-void editorSetStatusMessage(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
-    va_end(ap);
-    E.statusmsg_time = time(NULL);
-}
-
-
 void editorDrawRows(struct abuf *ab){
     int y;
     for(y = 0; y < E.screenrows; y++){
@@ -674,6 +644,38 @@ void editorDrawRows(struct abuf *ab){
         abAppend(ab, "\r\n", 2);
     }
 }
+
+void editorRefreshScreen(){
+    editorScroll();
+    struct abuf ab = ABUF_INIT;
+
+    abAppend(&ab,"\x1b[?25l", 6 );
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+    editorDrawStatusBar(&ab);
+    editorDrawMessageBar(&ab);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,(E.rx - E.coloff) + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
+void editorSetStatusMessage(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+    va_end(ap);
+    E.statusmsg_time = time(NULL);
+}
+
+
+
 
 
 
