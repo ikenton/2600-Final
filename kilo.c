4458076a707eb049dae204a5bd5357d2a4e00e35
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-09 12:14:28 -0800

    editorMoveCursor is above editorProcessKeypress to resolve wimplicit errors

--- kilo.c
+++ kilo.c
@@ -592,6 +592,44 @@ char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
         if (callback) callback(buf, c);
     }
 }
+
+void editorMoveCursor(int key){
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    switch(key){
+        case ARROW_LEFT:
+            if(E.cx != 0){
+                E.cx--;
+            }else if(E.cy > 0){
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            }
+            break;
+        case ARROW_RIGHT:
+            if (row && E.cx < row->size) {
+                E.cx++;
+            }else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
+            }
+            break;
+        case ARROW_UP:
+            if (E.cy != 0) {
+                E.cy--;
+            }
+            break;
+        case ARROW_DOWN:
+            if (E.cy != E.numrows) {
+                E.cy++;
+            }
+            break;
+    }
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
+    }
+}
+
 void editorProcessKeypress(){
     static int quit_times = KILO_QUIT_TIMES;
     int c = editorReadKey();
@@ -669,42 +707,7 @@ void editorProcessKeypress(){
     quit_times = KILO_QUIT_TIMES;
 }
 
-void editorMoveCursor(int key){
-    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
-    switch(key){
-        case ARROW_LEFT:
-            if(E.cx != 0){
-                E.cx--;
-            }else if(E.cy > 0){
-                E.cy--;
-                E.cx = E.row[E.cy].size;
-            }
-            break;
-        case ARROW_RIGHT:
-            if (row && E.cx < row->size) {
-                E.cx++;
-            }else if (row && E.cx == row->size) {
-                E.cy++;
-                E.cx = 0;
-            }
-            break;
-        case ARROW_UP:
-            if (E.cy != 0) {
-                E.cy--;
-            }
-            break;
-        case ARROW_DOWN:
-            if (E.cy != E.numrows) {
-                E.cy++;
-            }
-            break;
-    }
-    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
-    int rowlen = row ? row->size : 0;
-    if (E.cx > rowlen) {
-        E.cx = rowlen;
-    }
-}
+
 /* syntax highlighting */
 
 int is_separator(int c) {
