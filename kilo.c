db762b437206416314622f3acfe68053a5c53b79
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-09 12:10:38 -0800

    moved input and output sections below append to resolve some wimplicit errors

--- kilo.c
+++ kilo.c
@@ -223,6 +223,135 @@ int getWindowSize(int *rows, int *cols){
     }
 }
 
+
+
+/* editor operations */
+
+void editorInsertChar(int c) {
+    if (E.cy == E.numrows) {
+        editorInsertRow(E.numrows, "", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
+
+void editorInsertNewline() {
+    if (E.cx == 0) {
+        editorInsertRow(E.cy, "", 0);
+    } else {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
+}
+
+void editorDelChar() {
+    if (E.cy == E.numrows) return;
+    if (E.cx == 0 && E.cy == 0) return;
+    erow *row = &E.row[E.cy];
+    if (E.cx > 0) {
+        editorRowDelChar(row, E.cx - 1);
+        E.cx--;
+    } else {
+        E.cx = E.row[E.cy - 1].size;
+        editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+        editorDelRow(E.cy);
+        E.cy--;
+    }
+}
+
+/* find */
+void editorFindCallback(char *query, int key) {
+    static int last_match = -1;
+    static int direction = 1;
+
+    static int saved_hl_line;
+    static char *saved_hl = NULL;
+
+    if (saved_hl) {
+        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+
+    if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
+        return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
+    }
+
+    if (last_match == -1) direction = 1;
+    int current = last_match;
+    int i;
+    for (i = 0; i < E.numrows; i++) {
+        current += direction;
+        if (current == -1) current = E.numrows - 1;
+        else if (current == E.numrows) current = 0;
+
+        erow *row = &E.row[current];
+        char *match = strstr(row->render, query);
+        if (match) {
+            last_match = current;
+            E.cy = current;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+
+            saved_hl_line = current;
+            saved_hl = malloc(row->rsize);
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
+            break;
+        }
+    }
+}
+
+
+void editorFind() {
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+    int saved_rowoff = E.rowoff;
+
+    char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
+
+    if (query) {
+        free(query);
+    }else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloff = saved_coloff;
+        E.rowoff = saved_rowoff;
+  }
+}
+
+/* append buffer */
+struct abuf {
+  char *b;
+  int len;
+};
+
+
+void abAppend(struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab->len + len);
+
+    if(new == NULL) return;
+    memcpy(&new[ab -> len], s, len);
+    ab->b = new;
+    ab->len+=len;
+}
+
 /* output*/
 void editorDrawMessageBar(struct abuf *ab) {
   abAppend(ab, "\x1b[K", 3);
@@ -508,134 +637,6 @@ void editorMoveCursor(int key){
         E.cx = rowlen;
     }
 }
-
-/* editor operations */
-
-void editorInsertChar(int c) {
-    if (E.cy == E.numrows) {
-        editorInsertRow(E.numrows, "", 0);
-    }
-    editorRowInsertChar(&E.row[E.cy], E.cx, c);
-    E.cx++;
-}
-
-
-void editorInsertNewline() {
-    if (E.cx == 0) {
-        editorInsertRow(E.cy, "", 0);
-    } else {
-        erow *row = &E.row[E.cy];
-        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
-        row = &E.row[E.cy];
-        row->size = E.cx;
-        row->chars[row->size] = '\0';
-        editorUpdateRow(row);
-    }
-    E.cy++;
-    E.cx = 0;
-}
-
-void editorDelChar() {
-    if (E.cy == E.numrows) return;
-    if (E.cx == 0 && E.cy == 0) return;
-    erow *row = &E.row[E.cy];
-    if (E.cx > 0) {
-        editorRowDelChar(row, E.cx - 1);
-        E.cx--;
-    } else {
-        E.cx = E.row[E.cy - 1].size;
-        editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
-        editorDelRow(E.cy);
-        E.cy--;
-    }
-}
-
-/* find */
-void editorFindCallback(char *query, int key) {
-    static int last_match = -1;
-    static int direction = 1;
-
-    static int saved_hl_line;
-    static char *saved_hl = NULL;
-
-    if (saved_hl) {
-        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
-        free(saved_hl);
-        saved_hl = NULL;
-    }
-
-    if (key == '\r' || key == '\x1b') {
-        last_match = -1;
-        direction = 1;
-        return;
-    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
-    direction = 1;
-    } else if (key == ARROW_LEFT || key == ARROW_UP) {
-        direction = -1;
-    } else {
-        last_match = -1;
-        direction = 1;
-    }
-
-    if (last_match == -1) direction = 1;
-    int current = last_match;
-    int i;
-    for (i = 0; i < E.numrows; i++) {
-        current += direction;
-        if (current == -1) current = E.numrows - 1;
-        else if (current == E.numrows) current = 0;
-
-        erow *row = &E.row[current];
-        char *match = strstr(row->render, query);
-        if (match) {
-            last_match = current;
-            E.cy = current;
-            E.cx = editorRowRxToCx(row, match - row->render);
-            E.rowoff = E.numrows;
-
-            saved_hl_line = current;
-            saved_hl = malloc(row->rsize);
-            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
-            break;
-        }
-    }
-}
-
-
-void editorFind() {
-    int saved_cx = E.cx;
-    int saved_cy = E.cy;
-    int saved_coloff = E.coloff;
-    int saved_rowoff = E.rowoff;
-
-    char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
-
-    if (query) {
-        free(query);
-    }else {
-        E.cx = saved_cx;
-        E.cy = saved_cy;
-        E.coloff = saved_coloff;
-        E.rowoff = saved_rowoff;
-  }
-}
-
-/* append buffer */
-struct abuf {
-  char *b;
-  int len;
-};
-
-
-void abAppend(struct abuf *ab, const char *s, int len){
-    char *new = realloc(ab->b, ab->len + len);
-
-    if(new == NULL) return;
-    memcpy(&new[ab -> len], s, len);
-    ab->b = new;
-    ab->len+=len;
-}
-
 /* syntax highlighting */
 
 int is_separator(int c) {
