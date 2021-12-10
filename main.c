3c2eda19de57393594bc8464837c14cb09bafc57
ikenton <89114492+ikenton@users.noreply.github.com>
2021-12-09 21:22:27 -0800

    furiously debugging

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
-#include <stdio.h>//
-#include <stdlib.h>//
-//#include <sys/types.h>
 #include <sys/wait.h>//
 #include <unistd.h>//
+#include <stdlib.h>//
+#include <stdio.h>//
 #include <string.h>//
-
 
 int lsh_cd(char **args);
 int lsh_help(char **args);
@@ -16,74 +14,85 @@ char *builtin_str[] = {
     "exit"
 };
 
-int (*builtin_func[])(char **) = {
-    &lsh_cd, 
-    &lsh_help, 
+int (*builtin_func[]) (char **) = {
+    &lsh_cd,
+    &lsh_help,
     &lsh_exit
 };
 
-int lsh_num_builtins(){
-    return sizeof(builtin_str)/sizeof(char *);
+int lsh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
 }
 
-
-int lsh_cd(char **args){
-    if(args[1] == NULL){
-        fprintf(stderr, "lsh: expected arguement to \"cd\"\n");
-    }else {
-        if(chdir(args[1]) != 0){
-            perror("lsh");
+int lsh_cd(char **args)
+{
+    if (args[1] == NULL) {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+        perror("lsh");
         }
     }
     return 1;
 }
 
-int lsh_help(char **args){
+int lsh_help(char **args)
+{
     int i;
     printf("Stephen Brennan's LSH\n");
-    printf("Type program names and arguements, and hit enter.\n");
+    printf("Type program names and arguments, and hit enter.\n");
     printf("The following are built in:\n");
 
     for (i = 0; i < lsh_num_builtins(); i++) {
-        printf(" %s\n", builtin_str[i]);
+        printf("  %s\n", builtin_str[i]);
     }
+
     printf("Use the man command for information on other programs.\n");
     return 1;
 }
 
-int lsh_exit(char **args){
+
+int lsh_exit(char **args)
+{
     return 0;
 }
 
-int lsh_launch(char **args){
+int lsh_launch(char **args)
+{
     pid_t pid, wpid;
     int status;
+
     pid = fork();
-    if(pid == 0){
-        if(execvp(args[0], args) == -1){
-            perror("lsh");
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+        perror("lsh");
         }
         exit(EXIT_FAILURE);
-    }else if(pid <0){
+    } else if (pid < 0) {
+        // Error forking
         perror("lsh");
-    }else{
-        //fork exe successfully
-        do{
-            wpid = waitpid(pid, &status, WUNTRACED);
-        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    } else {
+        // Parent process
+        do {
+        wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
+
     return 1;
 }
 
-int lsh_execute(char **args){
-    int i; 
-    if(args[0] == NULL){
+int lsh_execute(char **args)
+{
+    int i;
+
+    if (args[0] == NULL) {
         return 1;
     }
 
-    for(i = 0; i < lsh_num_builtins(); i++){
-        if(strcmp(args[0], builtin_str[i]) == 0){
-            return(*builtin_func[i])(args);
+    for (i = 0; i < lsh_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+        return (*builtin_func[i])(args);
         }
     }
     return lsh_launch(args);
@@ -94,88 +103,87 @@ char *lsh_read_line(void)
 {
     int bufsize = LSH_RL_BUFSIZE;
     int position = 0;
-    char *buffer = malloc(sizeof(char)*bufsize);
+    char *buffer = malloc(sizeof(char) * bufsize);
     int c;
 
-    if(!buffer){
+    if (!buffer) {
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    while(1){
+    while (1) {
         c = getchar();
-        if(c = EOF || c == '\n'){
-            buffer[position] = '\0';
-            return buffer;
-        }else{
-            buffer[position] = c;
+        if (c == EOF || c == '\n') {
+        buffer[position] = '\0';
+        return buffer;
+        } else {
+        buffer[position] = c;
         }
         position++;
-    
 
-        if(position >= bufsize){
-            bufsize += LSH_RL_BUFSIZE;
-            buffer = realloc(buffer, bufsize);
-            if(!buffer){
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
+        if (position >= bufsize) {
+        bufsize += LSH_RL_BUFSIZE;
+        buffer = realloc(buffer, bufsize);
+        if (!buffer) {
+            fprintf(stderr, "lsh: allocation error\n");
+            exit(EXIT_FAILURE);
+        }
         }
     }
 }
 
 #define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM "\t\r\n\a"
-char **lsh_split_line(char *line){
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char *line)
+{
     int bufsize = LSH_TOK_BUFSIZE, position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
     char *token;
 
-    if(!tokens){
+    if (!tokens) {
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
     }
 
     token = strtok(line, LSH_TOK_DELIM);
-    while(token != NULL){
+    while (token != NULL) {
         tokens[position] = token;
         position++;
-        
-        if(position >= bufsize){
-            bufsize += LSH_TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if(!tokens){
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
+
+        if (position >= bufsize) {
+        bufsize += LSH_TOK_BUFSIZE;
+        tokens = realloc(tokens, bufsize * sizeof(char*));
+        if (!tokens) {
+            fprintf(stderr, "lsh: allocation error\n");
+            exit(EXIT_FAILURE);
         }
-        
+        }
+
         token = strtok(NULL, LSH_TOK_DELIM);
     }
     tokens[position] = NULL;
     return tokens;
 }
 
-
-
-
-void lsh_loop(void){
+void lsh_loop(void)
+{
     char *line;
     char **args;
     int status;
 
-    do{
+    do {
         printf("> ");
         line = lsh_read_line();
         args = lsh_split_line(line);
         status = lsh_execute(args);
-        
+
         free(line);
         free(args);
-    }while(status);
+    } while (status);
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     lsh_loop();
     return EXIT_SUCCESS;
 }
